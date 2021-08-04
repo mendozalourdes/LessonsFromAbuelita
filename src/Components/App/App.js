755a3d279a58350ba8f1aad260e4343a90a4b08a
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-04 16:16:53 -0500

    Create file structure for components and files

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import { Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+         Lessons from Abuelita
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
