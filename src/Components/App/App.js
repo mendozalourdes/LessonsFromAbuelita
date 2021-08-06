638e779103259f0c02b5254003499b14b8cacfc0
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-06 14:54:05 -0600

    Add logic for submitting sentences

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import WordsoftheDay from '../WordsoftheDay/WordoftheDay';
-
+import FormView from '../FormView/FormView';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          {/* <Home/> */}
-         <WordsoftheDay/>
+         {/* <WordsoftheDay/> */}
+         <FormView/>
       </header>
     </div>
   )
--- src/Components/FormView/FormView.js
+++ src/Components/FormView/FormView.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
+import "./FormView.css"; 
 
 class FormView extends React.Component {
-    constructor() {
+    constructor(props) {
+        console.log("props", props)
         super();
         this.state = {
             savedWord: [],
-            savedSentence: [],
+            sentence: [],
+            savedSentences: [],
             id: Date.now(),
             error: "",
         };
@@ -24,7 +26,55 @@ class FormView extends React.Component {
 
 
 
+    handleChange = event => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    submitSentence = event => {
+        event.preventDefault();
+        const newSentence = this.state.sentence
+        this.setState({ savedSentences:[...this.state.savedSentences,  newSentence] })
+        console.log("state", this.state)
+    }
+
+    render() {
+        console.log("saaaad", this.state)
+
+        return (
+
+<section className="homepage-view">
+<header className="app-header">
+    <h1 className="app-title">Lessons from Abuelita </h1>
+    <div className="links">
+    <p className="saved-link">Saved Words & Sentences</p>
+    <p className="origin-story-link">About Us</p>
+    </div>
+</header>
+    <section className="form-container">
+        <main className="main-form-container">
+            <input
+                id="sentence-input"
+                type="text"
+                name="sentence"
+                placeholder="Write your sentence here"
+                // value={this.state.name}
+                onChange={event => this.handleChange(event)}
+            /> 
+        <button id="submitBtn" className="submit-button" onClick={event => this.submitSentence(event)}>Send to Abuelita!</button>
+
+        </main>
+        </section>
+</section> 
+        )
+    }
+
+
+
+
+
+
 
 }
+
 
 export default FormView;
