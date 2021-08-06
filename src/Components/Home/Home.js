0ae9a2d5803f2ec386241de4e1dbeaac165c49b2
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-05 18:50:11 -0600

    Add information about the website on home view

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,18 +3,23 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div className="homepage-view">
-            <div className="app-header">
+        <section className="homepage-view">
+            <header className="app-header">
                 <h1>Lessons from Abuelita </h1>
                 <div className="links">
                 <p className="saved-link">Saved Words & Sentences</p>
                 <p className="origin-story-link">About Us</p>
                 </div>
-            </div>
-            <section className="main-container">
-
-            </section>
-        </div>
+            </header>
+            <main className="main-container">
+                <section className="main-section">
+                    <h3>
+                        Welcome to <h2>Lessons from Abuelita!</h2> This is a place where Abuelitas and nietes (grandkids) can come together to practice their Spanish together, share their culture, and create a beautiful connection that transcends borders and distance. 
+                    </h3>
+                    <button>Let's start learning!</button>
+                </section>
+            </main>
+        </section>
     )
 }
 
