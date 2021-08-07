4a617c682c00e036eba1965aa629c2308151ae00
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-07 16:17:39 -0600

    Add links to all buttons

--- src/Components/SingleWord/SingleWord.js
+++ src/Components/SingleWord/SingleWord.js
@@ -5,7 +5,6 @@ import { NavLink } from "react-router-dom";
 
 
 const SingleWord = ({word, definition, image, newWord}) => {
-console.log("newWord", newWord)
     return (
 
  <section className="homepage-view">
