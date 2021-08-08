48b5eda39ec58be845002f09f3a6762174e65869
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-08 14:58:40 -0600

    Add stub for test

--- cypress/integration/homePageView_spec.js
+++ cypress/integration/homePageView_spec.js
@@ -1,3 +1,9 @@
+const oneWord = {
+  word: 'discriminar',
+  DefinitionMD: " Dar trato de inferioridad a una persona o colectividad por motivos raciales,  religiosos,  de sexo,  de clase social o casta u otros motivos ideológicos:discriminar a una persona por cualquier motivo es un acto inconstitucional.  Separar,  diferenciar una cosa de otra",
+  image: "https://palabras-aleatorias-public-api.herokuapp.com/word-as-image?word=discriminar"
+}
+
 describe("Main Page View", () => {
     beforeEach(() => {
       cy.visit("http://localhost:3000");
@@ -9,17 +15,41 @@ describe("Main Page View", () => {
       cy.get("h1").contains("Lessons from Abuelita");
     });
 
-    it("Should show get data from API", () => {
-    cy.intercept('GET', 'https://palabras-aleatorias-public-api.herokuapp.com/random', {
-        statusCode: 200, 
-        fixture: 'word.json'
-    })
-    cy.get('#startLearningBtn').click()
-    cy.get('#randomWord')
-    cy.get('#wordImage')
-    cy.get('#formViewBtn')
-    cy.get('#getNewWord')
-    })
+    // it("Should show get data from API", () => {
+    //   cy.get('#startLearningBtn').click()
+    // cy.intercept('https://palabras-aleatorias-public-api.herokuapp.com/palabras-aleatorias?Word=discriminar', {
+    //     fixture: 'word.json'})
+
+    // // cy.visit("http://localhost:3000");
+    // // cy.wait('@wordReques t');
+    // cy.contains('a', oneWord.word).should('be.visible');
+
+    // // cy.get('#randomWord')
+    // // cy.get('#wordImage')
+    // // cy.get('#formViewBtn')
+    // // cy.get('#getNewWord')
+    // })
+
+
+    it('Should have a 201 status code', () => {
+      cy.visit("http://localhost:3000/word")
+      cy.get("h1").contains("Lessons from Abuelita");
+      cy.intercept(
+        {
+            method: "GET",
+            url: "https://palabras-aleatorias-public-api.herokuapp.com/palabras-aleatorias?Word=discriminar"
+        }, 
+        {
+            status: 201,
+            body: [{
+              word: 'discriminar',
+              DefinitionMD: " Dar trato de inferioridad a una persona o colectividad por motivos raciales,  religiosos,  de sexo,  de clase social o casta u otros motivos ideológicos:discriminar a una persona por cualquier motivo es un acto inconstitucional.  Separar,  diferenciar una cosa de otra",
+              image: "https://palabras-aleatorias-public-api.herokuapp.com/word-as-image?word=discriminar"
+            }]
+        }) 
+        cy.get('#randomWord')
+});
+
   
   
     it("Should have instructions for the user", () => {
@@ -46,11 +76,7 @@ describe("Main Page View", () => {
         cy.get('#wordImage')
         cy.get('#formViewBtn')
         cy.get('#getNewWord')
-
       });
   
-
-
-
   });
   
