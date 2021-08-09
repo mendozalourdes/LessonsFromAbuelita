3f14037439d5d4f3ed4ea57fc0667aef3512fb89
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-08 21:16:15 -0600

    Adjust error page component

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -2,34 +2,38 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import UserError from '../../assets/UserError.png' 
 import PropTypes from 'prop-types';
-
+import papelPicado from "../../assets/papelPicado.jpg";
+import './Error.css'
 
 const Error = ({errorMsg}) => {
     return (
      
          <section className="homepage-view">
       <header className="app-header">
+      <article className="heading-section-image-text">
+          <img className="papel-picado" alt="papel-picado" src={papelPicado}></img>
         <NavLink to={"/"}>
           <button className="title-button">
-            <h1 className="app-title">Lessons from Abuelita </h1>
+            Lessons from Abuelita 
           </button>
         </NavLink>
+        </article>
       </header>
-      <section>
-          <h2>{errorMsg}</h2>
+      <section className="error-message-container">
+          <h2 className="error-text">{errorMsg}</h2>
+          <div>
+              {errorMsg ===
+                "That page does not exist. Go back home?" && (
+                  <NavLink to='/'> Take Me to Abuelita's Home</NavLink>
+                  )}
+          </div>
           <img
                     className="error-image"
                     alt={"User-Error-Image"}
                     src={UserError}
                     id="userErrorImg"
                   ></img>
-      </section>
-      <div>
-          {errorMsg ===
-            "That page does not exist. Go back home?" && (
-            <NavLink to='/'> Take Me to Abuelita's Home</NavLink>
-          )}
-      </div>
+              </section>
       </section>
       );
     };
